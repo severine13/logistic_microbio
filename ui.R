90d3a8722f340a8996bc6e4169c88b730b77aeff
Severine Martini <martini.severine@gmail.com>
2017-11-12 20:05:13 +0100

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,38 @@
+#
+#
+
+library(shiny)
+
+
+shinyUI(fluidPage(
+  headerPanel("Welcome on a growth curve app"),
+# load dataset
+titlePanel("Upload file"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput('file1', 'Choose CSV File',
+                accept=c('text/csv', 
+                         'text/comma-separated-values,text/plain', 
+                         '.csv', '.txt')),
+      tags$hr(),
+      checkboxInput('header', 'Header', TRUE),
+      radioButtons('sep', 'Separator',
+                   c(Comma=',',
+                     Semicolon=';',
+                     Tab='\t'),
+                   ','),
+      radioButtons('quote', 'Quote',
+                   c(None='',
+                     'Double Quote'='"',
+                     'Single Quote'="'"),
+                   '"')
+    ),
+    
+    mainPanel(tableOutput('contents'),
+         
+      plotOutput('plot')
+    )
+  )
+  
+))
+
