3faac7237c338d2c1e790096dc8f6614f7878d9d
severinemartini <smartini@Nomadic.shore.mbari.org>
2017-11-30 11:53:47 +0100

    updates

--- ui.R
+++ ui.R
@@ -5,11 +5,11 @@ library(shiny)
 
 
 shinyUI(fluidPage(
-  headerPanel("Application of a logistic model on microbial datasets"),
+  headerPanel("Application to represent a logistic model on microbial datasets"),
 # load dataset
 titlePanel("Upload file"),
   sidebarLayout(
-    sidebarPanel("This is to perform logistic regression to estimate growth rate and maxium cells density.",
+    sidebarPanel("This application performs logistic regression to estimate growth rate and maxium cells density.",
       fileInput('file1', 'Choose CSV File',
                 accept=c('text/csv', 
                          'text/comma-separated-values,text/plain', 
@@ -34,7 +34,7 @@ titlePanel("Upload file"),
       withMathJax(),
       helpText('Logistic equation $$x(t) =r.x_0.(1-\\frac{x_0}K)$$'),
       tabsetPanel(
-      tabPanel("Model detail", helpText('You have to format data with tab separartor and dot for decimal. Remove all space in the header from your dataset. Replace it by "_"'),
+      tabPanel("Formating detail", helpText('You have to format data with tab separator and dot for decimal. Remove all spaces in the header from your dataset. Replace it by "_"'),
                helpText('In your data set there are two arrays, time and cells density (Optic density, Cells number, Biomass ....) ')),
       tabPanel("Plot",plotOutput('plot'), verbatimTextOutput('ex_out'))
       )
